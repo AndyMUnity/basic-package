739c6423280a0e7fb8229233d6dcac7503ccfaea
Andy Milsom <andrewmi@unity3d.com>
2019-09-11 14:48:41 +0100

    Test script

---
+++ Runtime/LogOnStart.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace TestPackage
+{
+    public class LogOnStart : UIBehaviour
+    {
+        void Start()
+        {
+            Debug.Log( "Im a monoBehaviour" );
+        }
+    }
+}
